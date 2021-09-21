ab6127bfc2986107449f1e16f3f97583dea4c6d6
Austen Wade <austen.wade.ut@gmail.com>
2021-09-21 17:49:25 -0600

    init commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/austen-wade/go-graphql
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/render v1.0.1 // indirect
+	github.com/graphql-go/graphql v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/austen/go-graphql/gql"
+	"github.com/austen/go-graphql/postgres"
+	"github.com/austen/go-graphql/server"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"github.com/graphql-go/graphql"
+)
+
+func main() {
+	router, db := initializeAPI()
+	defer db.Close()
+
+	log.Fatal(http.ListenAndServe(":4000", router))
+}
+
+func initializeAPI() (*chi.Mux, *postgres.Db) {
+	router := chi.NewRouter()
+
+	db, err := postgres.New(
+		postgres.ConnString("localhost", 5432, "austen", "go_graphql_db"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rootQuery := gql.NewRoot(db)
+	sc, err := graphql.NewSchema(
+		graphql.SchemaConfig{Query: rootQuery.Query},
+	)
+	if err != nil {
+		fmt.Println("Error creating schema: ", err)
+	}
+
+	s := server.Server{
+		GqlSchema: &sc,
+	}
+
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON),
+		middleware.Logger,
+		middleware.Compress(5, "gzip"),
+		middleware.StripSlashes,
+		middleware.Recoverer,
+	)
+
+	router.Post("/graphql", s.GraphQl())
+
+	return router, db
+}
